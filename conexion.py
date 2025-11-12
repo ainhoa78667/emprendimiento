7d6486aee779520841253f7b4194787871cf1253
ainhoa78667 <ainhoaalmanza2007@cesanrafael.org>
2025-11-12 15:15:32 -0400

    guardar2

--- conexion.py
+++ conexion.py
@@ -2,10 +2,10 @@ import mysql.connector
 
 def conectar():
     conexion = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1234",
-        database="sistema_cobros"
+        host="bmf20zlghgsykdwrepqz-mysql.services.clever-cloud.com",
+        user="u7ygwvuuyrm14duz",
+        password="DMG2kzTOgiWHQei2QJtx",
+        database="bmf20zlghgsykdwrepqz"
     )
     if conexion.is_connected():
         print("✅ Conexión exitosa a MySQL")
